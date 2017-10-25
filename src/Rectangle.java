b391b81d382870726af609ade20e605bbc57e89a
dimdung80 <32460035+dimdung80@users.noreply.github.com>
2017-10-25 01:57:53 -0400

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -6,11 +6,12 @@ public class Rectangle {
     this.length = length;
     this.width = width;
   }
+
   public int getArea() {
     return length * width;
   }
+
   public int getPerimeter() {
     return 2 * (length + width);
   }
-
 }
